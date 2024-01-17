c0d68d80ef831c9876b6e49d7ee3c55f948aff8e
Anatoli Belchykau <a.belchikov@totalgames.io>
2024-01-18 02:08:32 +0300

    Replaced few modules

--- main.py
+++ main.py
@@ -1,58 +1,71 @@
-import pyttsx3
 import speech_recognition as sr
 from decouple import config
 from datetime import datetime
 from openai import OpenAI
-import playsound
-import wave
+from time import sleep
+# import wave
+import pygame
+import nltk
+
+nltk.download('punkt')
+
+from nltk.tokenize import sent_tokenize
 
 
-# engine = pyttsx3.init('nsss') ## Mac OS
-# engine = pyttsx3.init('sapi5') ## Windows
-# engine = pyttsx3.init('espeak')
-
-# engine.setProperty('volume', 1.0)
-# engine.setProperty('voice', engine.getProperty('voices')[int(config('VOICE'))])
-
-clientOpenAI = OpenAI(api_key=config('OPEN_AI_API_KEY'))
-chatMessages = [
-    {"role": "system", "content": "You are a helpful voice assistant, which responds in short concise sentences, which will be read in less than 10 seconds until not asked for a precise answer."}
+pygame.mixer.init()
+client_open_ai = OpenAI(api_key=config('OPEN_AI_API_KEY'))
+chat_messages = [
+    {
+        "role": "system", 
+        "content": """
+            You are a helpful voice assistant, 
+            which responds in short concise sentences, 
+            which will be read in less than 3-5 seconds 
+            until not asked for a precise answer. 
+            Refer to the question parts in case if the question is related to a new topic.
+        """
+    }
 ]
 
 
-def speak(text):
-    """Used to speak whatever text is passed to it"""
-    engine.say(text)
-    engine.runAndWait()
+def speak_sentence(text):
+    global client_open_ai
 
-
-def speak2(text):
-    global clientOpenAI
-
-    response = clientOpenAI.audio.speech.create(
+    response = client_open_ai.audio.speech.create(
         model="tts-1",
         voice="alloy",
         input=text
     )
 
-    response.stream_to_file('/tmp/speech.mp3')
-    playsound.playsound('/tmp/speech.mp3', True)
+    while pygame.mixer.music.get_busy():
+        sleep(0.01)
+
+    response.stream_to_file('speech.mp3')
+    pygame.mixer.music.load("speech.mp3")
+    pygame.mixer.music.play()
+
+
+def speak(text):
+    sentences = sent_tokenize(text)
+
+    for i, sentence in enumerate(sentences):
+        try:
+            speak_sentence(sentence.strip(), i == (len(sentences) - 1))
+        except:
+            sleep(0.1)
 
 
 def greet_user():
-    return
-
-    hour = datetime.now().hour
-    speak2(f"I am {config('BOTNAME')}. How can i hale you today?")
+    speak(f"I am {config('BOTNAME')}. How can I help you today?")
 
 
 def recognize(audio):
-    global clientOpenAI
-    with open("/tmp/audio.wav", "wb") as file:
+    global client_open_ai
+    with open("audio.wav", "wb") as file:
         file.write(audio.get_wav_data())
 
-    audio_file = open('/tmp/audio.wav', 'rb')
-    transcript = clientOpenAI.audio.transcriptions.create(
+    audio_file = open('audio.wav', 'rb')
+    transcript = client_open_ai.audio.transcriptions.create(
         model="whisper-1", 
         file=audio_file
     )
@@ -62,20 +75,29 @@ def recognize(audio):
 
 
 def take_user_input():
+    while pygame.mixer.music.get_busy():
+        sleep(0.1)
+    sleep(0.1)
+
     r = sr.Recognizer()
     mic = sr.Microphone()
+
     with mic as source:
+        print('Calibrating...')
+        r.adjust_for_ambient_noise(source)
+
+        print('\007')
         print('Listening....')
-        r.pause_threshold = 1
+        r.pause_threshold = 2
         audio = r.listen(source)
 
     try:
         print('Recognizing...')
-        # query = r.recognize_google(audio, language=config('LANGUAGE'))
+        print('\007')
         query = recognize(audio)
 
         if 'exit' in query or 'stop' in query:
-            speak2('Have a good day sir!')
+            speak('Have a good day sir!')
             exit()
 
         return query
@@ -85,22 +107,34 @@ def take_user_input():
     return query
 
 
+def get_next_message(query):
+    global chat_messages
+    chat_messages.append({
+        "role": "user", 
+        "content": query
+    })
+    response = client_open_ai.chat.completions.create(
+        model='gpt-3.5-turbo', 
+        messages=chat_messages
+    )
+    answer = response.choices[0].message
+    chat_messages.append(
+        answer
+    )
+    return answer.content
+
+
 if __name__ == '__main__':
     greet_user()
+
     while True:
         query = take_user_input().lower()
 
         if not query:
             continue
 
-        print(query)
+        print('Question:', query)
+        answer = get_next_message(query)
 
-        chatMessages.append({"role": "user", "content": query})
-
-        response = clientOpenAI.chat.completions.create(model='gpt-4', messages=chatMessages)
-
-        chatMessages.append(response.choices[0].message)
-
-        print(f'Answer: {response.choices[0].message.content}')
-
-        speak2(response.choices[0].message.content)
+        print(f'Answer:', answer)
+        speak(answer)
